e1ac2d18ddceaecd7ecfbdf050ba94540032c699
Pankaj Doharey <pankajdoharey@gmail.com>
2012-08-18 18:08:54 +0530

    Added Back clock to terminal and made it super efficient

--- .custom_scripts/utils/clock.sh
+++ .custom_scripts/utils/clock.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+let x_cols=$(($(tput cols)-44))
 
-function header_clock {
-		tput cuu1
-		tput sc
-		let x_cols=$(tput cols)-33
-		tput cup 0 ${x_cols}
-		tput setaf 4 ; tput bold
-		echo -n "["
-		tput setaf 1
-		echo -n "$(date +%c)"
-		tput setaf 4 ; tput bold
-		echo -n "]"
-		tput rc
-}
+while :
+do
+		Time=`date +%c`
+		Date=`date +"%d-%m-%Y"`
+		#save current screen position & attributes eith tput sc
+		echo -en "$(tput sc;tput cup 0 ${x_cols};tput bold)$(tput setaf 1;tput setaf 7;tput setab 1)[$Time \033[46m$Date]\033[0m$(tput sgr0;tput rc)"
+  #echo -e -n "\033[u"
+		
+ #Delay for 1 second
+	sleep 1
+done &
+#restore current screen position & attributes
