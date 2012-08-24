0fceeb165195c0633578487064d29ce532563870
Pankaj Doharey <pankajdoharey@gmail.com>
2012-08-24 19:05:56 +0530

    Meta key combo M-i for begin

--- .custom_scripts/libs/bind_key.sh
+++ .custom_scripts/libs/bind_key.sh
@@ -3,13 +3,13 @@
 declare -A KEY
 
 KEY=(
-		[CTRL]=\\C
-		[META]=\\e
-		[ENTER]=\\C-j
+		[CTRL]="\C"
+		[META]="\e"
+		[ENTER]="\n"
     )
 
 #string="'\"\C-i\":\"begin\C-m\"'"
 
-key_string="\"${KEY[CTRL]}-i\":\"begin${KEY[ENTER]}\""
+key_string="\"${KEY[META]}i\":\"begin${KEY[ENTER]}\""
 
 bind $key_string
